5f507bba2ba1cdd891b14b1cdf33a06b4df9e95e
shizhonghao <shi1996426frank@126.com>
2017-06-01 20:52:25 +0800

    added communications functions

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ import sys
 import time
 import threading
 import xml.dom.minidom as Dom
+import re
+import math
 
 class communicate:
     def __init__(self):
@@ -10,13 +12,6 @@ class communicate:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
             # Connect to server and send data
         self.sock.connect((self.HOST, self.PORT))
-        
-    def send(self,info):
-        data = info
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        
-        self.sock.sendall(bytes(data + "\n", "utf-8"))
-        print("Sent:     {}".format(data))
 
     def AC_Req(self,Positive,Wind_Level):
         doc = Dom.Document() 
@@ -30,10 +25,114 @@ class communicate:
         node_Wind.appendChild(doc.createTextNode(str(Wind_Level)))
         root.appendChild(node_Wind)
 
-        self.send(str(len(root.toxml())) + root.toxml())
+        self.send(str(len(root.toxml()) + 1) + root.toxml())
+
+    def Temp_Submit(self,Time,Client_No,Temp):
+        doc = Dom.Document()
+        root = doc.createElement("Temp_Submit")
+
+        node_Time = doc.createElement("Positive")
+
+    def Login(self,Name,Password,Client_No):
+        doc = Dom.Document()
+        root = doc.createElement("Login")
+
+        node_Name = doc.createElement("Name")
+        node_Name.appendChild(doc.createTextNode(str(Name)))
+        root.appendChild(node_Name)
+
+        node_Pass = doc.createElement("Password")
+        node_Pass.appendChild(doc.createTextNode(str(Password)))
+        root.appendChild(node_Pass)
+
+        node_CNO = doc.createElement("Client_No")
+        node_CNO.appendChild(doc.createTextNode(str(Client_No)))
+        root.appendChild(node_CNO)
+
+        self.send(str(len(root.toxml()) + 1) + root.toxml())
+
+    # ------local setting functions
+    def Login_ACK(self,Succeed,Password,Mode):
+        pass
+
+    def Mode(self,Heater):
+        pass
+
+    def Wind(self,Level,Start_Blowing):
+        pass
+
+    def Fare_Info(self,Fare,Energy):
+        pass
+
+    def Temp_Submit_Freq(self,Temp_Submit_Freq):
+        pass
+
+    # ------info processing functions
+    def parse(self,xml):
+        print("parse xml:",xml)
+        root = Dom.parseString(xml).documentElement
+
+        if (root.nodeName == "Login_ACK"):
+            node = root.getElementsByTagName("Succeed")
+            Succeed = node[0].childNodes[0].data
+
+            node = root.getElementsByTagName("Name")
+            Name = node[0].childNodes[0].data
+
+            node = root.getElementsByTagName("Password")
+            Password = node[0].childNodes[0].data
+
+            node = root.getElementsByTagName("Mode")
+            Mode = node[0].childNodes[0].data
+
+            self.Login_ACK(Succeed,Name,Password,Mode)
+
+        if (root.nodeName == "Mode"):
+            node = root.getElementsByTagName("Succeed")
+            Heater = node[0].childNodes[0].data
+
+            self.Login_ACK(Heater)
+
+        if (root.nodeName == "Wind"):
+            node = root.getElementsByTagName("Level")
+            Level = node[0].childNodes[0].data
+
+            node = root.getElementsByTagName("Start_Blowing")
+            Start_Blowing = node[0].childNodes[0].data
+
+            self.Login_ACK(Level,Start_Blowing)
+
+        if (root.nodeName == "Fare_Info"):
+            node = root.getElementsByTagName("Fare")
+            Fare = node[0].childNodes[0].data
+
+            node = root.getElementsByTagName("Energy")
+            Energy = node[0].childNodes[0].data
+
+            self.Fare_Info(Fare,Energy)
+
+        if (root.nodeName == "Temp_Submit_Freq"):
+            node = root.getElementsByTagName("Temp_Submit_Freq")
+            Temp_Submit_Freq = node[0].childNodes[0].data
+
+            self.Temp_Submit_Freq(Temp_Submit_Freq)
 
     def recv(self,data):
-        pass
+        print("process:", data)
+        while (data):
+            print("in recv")
+            xml_len = int(re.findall('^[0-9]+', data)[0])
+            head_len = int(math.log(xml_len, 10)) + 1
+            xml = data[head_len:head_len + xml_len]
+            print("xml:", xml)
+            self.parse(xml)
+            data = data[1 + head_len + xml_len:]
+            print("data:", data)
+
+    def send(self, info):
+        self.sock.sendall(bytes(info + "\n", "utf-8"))
+        print("Sent:     {}".format(info))
+
 
 class client_thread(threading.Thread):
     def __init__(self,client):
@@ -53,9 +152,9 @@ print('通信类已建立Client')
 if __name__=="__main__":
     
     c.AC_Req(1,2)
-    #c.AC_Req(1,3)
+    c.Login("frank","123456789",103)
     i=1
     while(True):
-        time.sleep(2)
+        time.sleep(7)
         i = i + 1
         c.send(str(str(8) + "<t>" + str(i) + "<\t>"))
--- server.py
+++ server.py
@@ -13,23 +13,47 @@ class communicate:
         self.soc.bind((self.HOST, self.PORT))
         self.soc.listen(10) #max number of clients listening to
         self.socket_list = []
+        self.room_dict = {}
 
+    #------local setting functions
     def AC_Req(self,Positive,Wind_Level):
         print("in req")
         print("AC_Req",Positive,Wind_Level)
 
+    def Temp_Submit(self,Time,Client_No,Temp):
+        pass
+
+    def Login(self,Name,PassWord,Client_No):
+        pass
+
+    #------info processing functions
     def parse(self,xml):
         root = Dom.parseString(xml).documentElement
         #print(root.nodeName)
-        if(root.nodeName=='AC_Req'):
-            node = root.getElementsByTagName('Positive')
+        if(root.nodeName == "AC_Req"):
+            node = root.getElementsByTagName("Positive")
             Positive = node[0].childNodes[0].data
 
-            node = root.getElementsByTagName('Wind_Level')
+            node = root.getElementsByTagName("Wind_Level")
             Wind_Level = node[0].childNodes[0].data
             
             self.AC_Req(Positive,Wind_Level)
-    
+
+        if(root.nodeName == "Temp_Submit"):
+            pass
+
+        if(root.nodeName == "Login"):
+            node = root.getElementsByTagName("Name")
+            Name = node[0].childNodes[0].data
+
+            node = root.getElementsByTagName("Password")
+            Password = node[0].childNodes[0].data
+
+            node = root.getElementsByTagName("Client_No")
+            Client_No = node[0].childNodes[0].data
+
+            self.Login(Name,Password,Client_No);
+
     def recv(self,data,conn,addr):
         print("process:",data)
         data = data.decode()
@@ -42,7 +66,21 @@ class communicate:
             self.parse(xml)
             data = data[1+head_len+xml_len:]
             print("data:",data)
-        
+
+    def send(self,no,info):  # no can be a room number or a socket(before Login_ACK)
+        if type(no) == int:  # no is a room number
+            try:
+                sock = self.room_dict[no]
+                sock.sendall(bytes(info + "\n", "utf-8"))
+            except:
+                print(no,"is not connected")
+        else:  # no is a socket
+            try:
+                sock = no
+                sock.sendall(bytes(info + "\n", "utf-8"))
+            except:
+                print(no,"connection error")
+
     def broadcast(self,info):
         for (conn,addr) in self.socket_list:
             print("send:",info)
@@ -51,6 +89,87 @@ class communicate:
             except: #disconnected
                 print("disconnected:",addr)
                 self.socket_list.remove((conn,addr))
+
+    #------message sending functions
+    def Login_ACK(self,no,Succeed,Name,Password,Mode):
+        doc = Dom.Document()
+        root = doc.createElement("Login_ACK")
+
+        node_Succeed = doc.createElement("Succeed")
+        node_Succeed.appendChild(doc.createTextNode(str(Succeed)))
+        root.appendChild(node_Succeed)
+
+        node_Name = doc.createElement("Name")
+        node_Name.appendChild(doc.createTextNode(str(Name)))
+        root.appendChild(node_Name)
+
+        node_Password = doc.createElement("Password")
+        node_Password.appendChild(doc.createTextNode(str(Password)))
+        root.appendChild(node_Password)
+
+        node_Mode = doc.createElement("Mode")
+        node_Mode.appendChild(doc.createTextNode(str(Mode)))
+        root.appendChild(node_Mode)
+
+        self.send(no, str(len(root.toxml()) + 1) + root.toxml())
+
+    def Mode(self,no,Heater):
+        doc = Dom.Document()
+        root = doc.createElement("Mode")
+
+        node_Heater = doc.createElement("Heater")
+        node_Heater.appendChild(doc.createTextNode(str(Heater)))
+        root.appendChild(node_Heater)
+
+        self.send(no, str(len(root.toxml()) + 1) + root.toxml())
+
+    def Mode(self,Heater): #broadcast
+        doc = Dom.Document()
+        root = doc.createElement("Mode")
+
+        node_Heater = doc.createElement("Heater")
+        node_Heater.appendChild(doc.createTextNode(str(Heater)))
+        root.appendChild(node_Heater)
+
+        self.broadcast(str(len(root.toxml()) + 1) + root.toxml())
+
+    def Wind(self,no,Level,Start_Blowing):
+        doc = Dom.Document()
+        root = doc.createElement("Wind")
+
+        node_Level = doc.createElement("Level")
+        node_Level.appendChild(doc.createTextNode(str(Level)))
+        root.appendChild(node_Level)
+
+        node_Blow = doc.createElement("Start_Blowing")
+        node_Blow.appendChild(doc.createTextNode(str(Start_Blowing)))
+        root.appendChild(node_Blow)
+
+        self.send(no, str(len(root.toxml()) + 1) + root.toxml())
+
+    def Fare_Info(self,no,Fare,Energy):
+        doc = Dom.Document()
+        root = doc.createElement("Fare_Info")
+
+        node_Fare = doc.createElement("Fare")
+        node_Fare.appendChild(doc.createTextNode(str(Fare)))
+        root.appendChild(node_Fare)
+
+        node_Energy = doc.createElement("Energy")
+        node_Energy.appendChild(doc.createTextNode(str(Energy)))
+        root.appendChild(node_Energy)
+
+        self.send(no, str(len(root.toxml()) + 1) + root.toxml())
+
+    def Temp_Submit_Freq(self,Temp_Submit_Freq): #broadcast
+        doc = Dom.Document()
+        root = doc.createElement("Temp_Submit_Freq")
+
+        node_Freq = doc.createElement("Temp_Submit_Freq")
+        node_Freq.appendChild(doc.createTextNode(str(Temp_Submit_Freq)))
+        root.appendChild(node_Freq)
+
+        self.broadcast(str(len(root.toxml()) + 1) + root.toxml())
 
 class receiver_thread(threading.Thread):
     def __init__(self,conn,addr):
@@ -90,7 +209,10 @@ class connector_thread(threading.Thread):
 server = communicate()
 connector_thread().start()
 print('通信类已建立Server')
+
 if __name__ == "__main__":
     while(1):
         time.sleep(5)
-        server.broadcast("broadcast info")
+        server.Temp_Submit_Freq(5)
+        inp = input("press ENTER to send ACK")
+        server.Login_ACK(103,1,"frank","123456789","1")
